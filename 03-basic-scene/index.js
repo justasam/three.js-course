c188ff78ad08f210540ab1a295ee819f5690e2ee
justas <samuolis.just@gmail.com>
2021-03-13 13:58:04 +0200

    finish basic scene

--- 03-basic-scene/index.js
+++ 03-basic-scene/index.js
@@ -1,1 +1,24 @@
-console.log('Hello three.js')
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+const canvas = document.querySelector('canvas.webgl')
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
